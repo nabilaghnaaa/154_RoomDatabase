99655bdc5fb2f942672310c2783f3ff7856da6e0
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-05 09:53:24 +0700

    menambahkan rute DestinasiEntry untuk halaman input siswa

---
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.roomdatabase.view.route
+
+import com.example.roomdatabase.R
+import com.example.roomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route: String = "item_entry"
+    override val titleRes: Int = R.string.entry_siswa
+}
