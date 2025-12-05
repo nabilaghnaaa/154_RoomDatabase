f9d9ca7a424404cfca3cb154bfae90dc77d4e63c
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-05 10:25:58 +0700

    mengubah penamaan library

--- app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/roomdatabase/repositori/ContainerApp.kt
@@ -2,7 +2,7 @@ package com.example.roomdatabase.repositori
 
 import android.app.Application
 import android.content.Context
-import com.example.a20230140094_roomdatabase.room.DatabaseSiswa
+import com.example.roomdatabase.room.DatabaseSiswa
 
 interface ContainerApp{
     val repositoriSiswa : RepositoriSiswa
