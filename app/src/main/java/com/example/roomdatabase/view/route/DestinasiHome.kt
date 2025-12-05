9ccce547ffc67ae86df64f29fc1cc41447fe93a2
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-05 09:54:44 +0700

    menambahkan rute DestinasiHome untuk halaman utama

---
+++ app/src/main/java/com/example/roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase.view.route
+
+import com.example.roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route: String = "home"
+    override val titleRes: Int = R.string.app_name
+}
