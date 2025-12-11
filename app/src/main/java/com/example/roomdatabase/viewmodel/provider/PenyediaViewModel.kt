58399d885d0e8e8c16ef83ea3274d883ccd4e800
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-11 23:01:09 +0700

    penyesuaian di PenyediaViewModel.kt

--- app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,28 +1,33 @@
 package com.example.roomdatabase.viewmodel.provider
 
-import android.app.Application
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.roomdatabase.repositori.AplikasiSiswa
+import com.example.roomdatabase.viewmodel.DetailViewModel
+import com.example.roomdatabase.viewmodel.EditViewModel
 import com.example.roomdatabase.viewmodel.EntryViewModel
 import com.example.roomdatabase.viewmodel.HomeViewModel
 
-object PenyediaViewModel {
-
+object PenyediaViewModel{
     val Factory = viewModelFactory {
-
-        // --- HomeViewModel ---
         initializer {
-            HomeViewModel(
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
                 aplikasiSiswa().container.repositoriSiswa
             )
         }
-
-        // --- EntryViewModel ---
         initializer {
-            EntryViewModel(
+            EditViewModel(
+                this.createSavedStateHandle(),
                 aplikasiSiswa().container.repositoriSiswa
             )
         }
@@ -30,8 +35,9 @@ object PenyediaViewModel {
 }
 
 /**
- * Fungsi ekstensi untuk mengambil instance AplikasiSiswa dari CreationExtras
+ * Fungsi ekstensi query untuk objek Application dan mengembalikan sebuah instance dari
+ * AplikasiSiswa
  */
-fun CreationExtras.aplikasiSiswa(): AplikasiSiswa {
-    return (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
-}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
