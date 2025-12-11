299b7eb0d699d5a0eb2da9e1f92aae4d092d22fa
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-11 21:25:51 +0700

    memperbaiki main activity

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.roomdatabase
+package com.example.a20230140094_roomdatabase
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,19 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.roomdatabase.ui.theme.RoomDatabaseTheme
+import com.example.roomdatabase.view.uicontroller.SiswaApp
+import com.example.roomdatabase.view.uicontroller.SiswaApp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RoomDatabaseTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                SiswaApp(
+                    modifier = Modifier.padding(innerPadding)
+                )
             }
         }
     }
