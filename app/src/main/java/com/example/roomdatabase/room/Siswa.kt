404e1ef1ce3da9e1df7c276905d80b54339af3d2
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-11 23:01:48 +0700

    penyesuaian di penyesuaian di Siswa.kt

--- app/src/main/java/com/example/roomdatabase/room/Siswa.kt
+++ app/src/main/java/com/example/roomdatabase/room/Siswa.kt
@@ -3,12 +3,11 @@ package com.example.roomdatabase.room
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
 @Entity(tableName = "tblSiswa")
 data class Siswa(
     @PrimaryKey(autoGenerate = true)
-    val id : Int = 0,
-    val nama : String,
-    val alamat : String,
-    val telpon : String,
+    val id: Int = 0,
+    val nama: String,
+    val alamat: String,
+    val telpon: String
 )
