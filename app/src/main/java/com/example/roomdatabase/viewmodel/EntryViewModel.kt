f3cc605eb9c4cee7ea4891fdd0658941a5c4880b
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-11 22:59:59 +0700

    penyesuaian di EntryViewModel.kt

--- app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/example/roomdatabase/viewmodel/EntryViewModel.kt
@@ -7,30 +7,32 @@ import androidx.lifecycle.ViewModel
 import com.example.roomdatabase.repositori.RepositoriSiswa
 import com.example.roomdatabase.room.Siswa
 
-class EntryViewModel( private val repositoriSiswa: RepositoriSiswa) : ViewModel()
-{
-    var uiStateSiswa by mutableStateOf(UIStateSiswa())
-        private set
 
-    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa
-    ): Boolean {
-        return with(uiState) {
+class EntryViewModel(private val repositoriSiswa: RepositoriSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
+        return with(uiState){
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
 
-    fun updateUiState(detailSiswa: DetailSiswa) {
+    fun updateUiState(detailSiswa: DetailSiswa){
         uiStateSiswa =
-            UIStateSiswa(detailSiswa = detailSiswa, isEntryValid =
-                validasiInput(detailSiswa))
+            UIStateSiswa(detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa))
     }
 
-    suspend fun saveSiswa() {
-        if (validasiInput()) {
+    suspend fun saveSiswa(){
+        if (validasiInput()){
             repositoriSiswa.insertSiswa(uiStateSiswa.detailSiswa.toSiswa())
         }
     }
 }
+
+/**
+ * mewakili status UI saat ini
+ */
 
 data class UIStateSiswa(
     val detailSiswa: DetailSiswa = DetailSiswa(),
@@ -41,27 +43,30 @@ data class DetailSiswa(
     val id: Int = 0,
     val nama: String = "",
     val alamat: String = "",
-    val telpon: String = ""
+    val telpon: String = "",
 )
 
-fun DetailSiswa.toSiswa(): Siswa =
-    Siswa(
-        id = id,
-        nama = nama,
-        alamat = alamat,
-        telpon = telpon
-    )
+/**
+ * Fungsi untuk mengkonversi data input ke data dalam tabel sesuai jenis datanya
+ */
 
-fun Siswa.toDetailSiswa(): DetailSiswa =
-    DetailSiswa(
-        id = id,
-        nama = nama,
-        alamat = alamat,
-        telpon = telpon
-    )
 
-fun Siswa.toUIStateSiswa(isEntryValid: Boolean = false): UIStateSiswa =
-    UIStateSiswa(
-        detailSiswa = this.toDetailSiswa(),
-        isEntryValid = isEntryValid
-    )
+fun DetailSiswa.toSiswa(): Siswa = Siswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
+
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false)
+        : UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
+
+fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(
+    id = id,
+    nama = nama,
+    alamat = alamat,
+    telpon = telpon
+)
