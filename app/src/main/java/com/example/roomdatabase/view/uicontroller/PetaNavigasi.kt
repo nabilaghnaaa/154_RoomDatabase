120e1818d2cce1dca1fe50d878cb9b0bc22db0cf
nabilaghnaaa <nabilaghnaaa@gmmail.com>
2025-12-11 23:01:33 +0700

    penyesuaian di penyesuaian di PetaNavigasi.kt

--- app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -1,23 +1,28 @@
 package com.example.roomdatabase.view.uicontroller
-
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavArgument
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.roomdatabase.view.DetailSiswaScreen
+import com.example.roomdatabase.view.EditSiswaScreen
 import com.example.roomdatabase.view.EntrySiswaScreen
 import com.example.roomdatabase.view.HomeScreen
-import com.example.roomdatabase.view.route.DestinasiHome
+import com.example.roomdatabase.view.route.DestinasiDetailSiswa
+import com.example.roomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
+import com.example.roomdatabase.view.route.DestinasiEditSiswa
 import com.example.roomdatabase.view.route.DestinasiEntry
+import com.example.roomdatabase.view.route.DestinasiHome
+
 
 @Composable
-fun SiswaApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier = Modifier
-) {
-    HostNavigasi(navController = navController, modifier = modifier)
+fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -25,23 +30,50 @@ fun SiswaApp(
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = modifier
-    ) {
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
         composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiEntry.route)
-                }
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = { navController.navigate("${DestinasiDetailSiswa.route}/${it}") },
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
+
+        composable(
+            route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetailSiswa.itemIdArg) {
+                type = NavType.IntType
+            })
+        ) {
+            DetailSiswaScreen(
+                // ... kode lain ...
+
+                // PERBAIKAN DI SINI: Gunakan DestinasiEditSiswa
+                navigateToEditItem = { navController.navigate("${DestinasiEditSiswa.route}/$it") },
+
+                navigateBack = { navController.navigateUp() }
             )
         }
 
-        composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
-                navigateBack = { navController.popBackStack() }
+        composable(
+            route = DestinasiEditSiswa.routeWithArgs,
+            arguments = listOf(
+                navArgument(itemIdArg) {
+                    type = NavType.IntType
+                    defaultValue = -1 // Pilihan: Menambahkan defaultValue untuk keamanan
+                }
+            )
+        ) { backStackEntry -> // backStackEntry diperlukan jika Anda ingin mengakses argumen secara langsung di sini
+            EditSiswaScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+                // Anda mungkin perlu meneruskan argumen ke EditSiswaScreen
+                // itemId = backStackEntry.arguments?.getInt(itemIdArg) ?: -1
             )
         }
     }
